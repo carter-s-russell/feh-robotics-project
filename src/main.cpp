03b5974bc464541fb7e35a83c2053b945268106e
Carter Russell <carterrussellyt@gmail.com>
2026-02-27 13:46:59 -0500

    comment out components to compile

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,17 @@
 #include "../include/drivetrain.h"
 
 // declarations for encoders & motors
-DigitalEncoder right_encoder(FEHIO::Pin8);
-DigitalEncoder left_encoder(FEHIO::Pin9);
-FEHMotor right_motor(FEHMotor::Motor0,9.0);
-FEHMotor left_motor(FEHMotor::Motor1,9.0);
+//DigitalEncoder right_encoder(FEHIO::Pin8);
+//DigitalEncoder left_encoder(FEHIO::Pin9);
+//FEHMotor right_motor(FEHMotor::Motor0,9.0);
+//FEHMotor left_motor(FEHMotor::Motor1,9.0);
 
-// other declariations
-Drivetrain driveChain(FEHMotor::Motor0, FEHMotor::Motor1, FEHIO::Pin7, FEHIO::Pin8);
+// other declarations
+//Drivetrain driveChain(FEHMotor::Motor0, FEHMotor::Motor1, FEHIO::Pin7, FEHIO::Pin8);
 
 void ERCMain()
 {
-    driveChain.driveForward(6, 25);
+    //driveChain.driveForward(6, 25);
+    TestGUI();
 }
 
